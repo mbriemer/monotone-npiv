f4cf8094e34da100a07f1136da2b791f3ea6a366
mbriemer <82475855+mbriemer@users.noreply.github.com>
2023-08-24 12:37:43 +0200

    Add constrained optimization, basic sim evaluation

--- main.R
+++ main.R
@@ -3,7 +3,7 @@
 # Simulation parameters
 
 set_sim_parameters <- function(k, j, sigma = 0.3, rho = 0.5, eta = 0.5) {
-  sim_parameters <- list(k= k,
+  sim_parameters <- list(k = k,
                          j = j, #TODO enforce j >= k?
                          sigma = sigma,
                          rho = rho,
@@ -39,11 +39,11 @@ sample_x <- function(rho, zeta, lunate_epsilon) {
 }
 
 g <- function(x, model_no) {
-  if (model_no == 1){
+  if (model_no == 1) {
     return(x^2 + 0.2 * x)
   }
-  if (model_no == 2){
-    return(2 * max(0, x - 1 / 2)^2 + 0.5 * x)
+  if (model_no == 2) {
+    return(2 * pmax(0, x - 1 / 2)^2 + 0.5 * x)
   }
 }
 
@@ -55,26 +55,53 @@ sample_y <- function(x, epsilon, model_no) {
 # Estimation functions
 
 evaluate_basis <- function(x, basis_dimension) {
-  if (basis_dimension == 1){
+  if (basis_dimension == 1) {
     return(1)
   }
-  if (basis_dimension == 2){
+  if (basis_dimension == 2) {
     return(cbind(1, x))
   }
-  if (basis_dimension == 3){
+  if (basis_dimension == 3) {
     return(cbind(1, x, x^2))
   }
-  if (basis_dimension == 4){
-    return(cbind(1, x, x^2, max(0, x - 1 / 2)^2))
+  if (basis_dimension == 4) {
+    return(cbind(1, x, x^2, pmax(0, x - 1 / 2)^2))
   }
-  if (basis_dimension == 5){
-    return(cbind(1, x, x^2, max(0, x - 1 / 3)^2, max(0, x - 2 / 3)^2))
+  if (basis_dimension == 5) {
+    return(cbind(1, x, x^2, pmax(0, x - 1 / 3)^2, pmax(0, x - 2 / 3)^2))
   }
 }
 
-estimate <- function(b, y, x, w, p, q){
+calculate_minimand <- function(b, y, x, w, p, q) {
   minimand <- t(y - p %*% b) %*% q %*% solve(crossprod(q)) %*% t(q) %*% (y - p %*% b)
   return(minimand)
+}
+
+estimate <- function(y, x, w, p, q, constraint = "none") {
+  k <- ncol(p)
+  lower <- rep(-Inf, k)
+  upper <- rep(Inf, k)
+
+  if (constraint == "increasing") {
+    lower <- c(-Inf, rep(0, k - 1)) #TODO maybe more complex for j,k >= 4
+  }
+
+  if (constraint == "decreasing") {
+    upper <- c(Inf, rep(0, k - 1)) #TODO maybe more complex for j,k >= 4
+  }
+
+  beta <- optim(par = rep(1, k),
+                fn = calculate_minimand,
+                y = y,
+                x = x,
+                w = w,
+                p = p,
+                q = q,
+                method = "L-BFGS-B",
+                lower = lower,
+                upper = upper
+)
+  return(beta$par)
 }
 
 # Main
@@ -90,16 +117,36 @@ w <- sample_w(zeta = noise_terms$zeta)
 x <- sample_x(rho = sim_parameters$rho,
               zeta = noise_terms$zeta,
               lunate_epsilon = noise_terms$lunate_epsilon)
-y <- sample_y(x = x, epsilon = noise_terms$epsilon, model_no = 1)
+y <- sample_y(x = x, epsilon = noise_terms$epsilon, model_no = 2)
 
 p <- evaluate_basis(x, sim_parameters$k)
 q <- evaluate_basis(w, sim_parameters$j)
 
-minimize <- optim(par = rep(1, sim_parameters$k),
-                  fn = estimate,
-                  y = y,
-                  x = x,
-                  w = w,
-                  p = p,
-                  q = q,
-)
+beta_u <- estimate(y = y,
+                   x = x,
+                   w = w,
+                   p = p,
+                   q = q,
+                   constraint = "none")
+
+beta_c <- estimate(y = y,
+                   x = x,
+                   w = w,
+                   p = p,
+                   q = q,
+                   constraint = "increasing")
+
+# Simulation results
+
+y_hat_u <- p %*% beta_u
+y_hat_c <- p %*% beta_c
+
+plot(x, g(x, model_no = 2), col = "black")
+points(x, y_hat_u, col = "red")
+points(x, y_hat_c, col = "blue")
+
+# Calculate MSE
+# TODO Should be possible to find an analytic expression for the MISE?
+
+mse_u <- mean((y - y_hat_u)^2)
+mse_c <- mean((y - y_hat_c)^2)
